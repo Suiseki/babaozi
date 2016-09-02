16bfc67c6ef343619b76ab7b45b7267905375c14
Bartek <suiseki@gazeta.pl>
2016-09-02 07:40:55 +0200

    hiding and showing andwers navigation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # BaBaozi - simple flashcards
-This is a simple app, based a flash symbian version i wrore few year ago. All cards are stored in a text file. User clicks several buttons to draw a card and schow chinese characters, pronunciation or translation
+This is a simple app, based a flash version i wrote few year ago. All cards are stored in a text file. User clicks several buttons to draw a card and schow chinese characters, pronunciation or translation
 
 ## Feature list
 - parse json character base
@@ -8,5 +8,4 @@ This is a simple app, based a flash symbian version i wrore few year ago. All ca
 - draw random translation
 
 ### Credits
-  - Showdown v1
-    * [Bartosz Kamiński](https://twitter.com/Suiseki)
+* [Bartosz Kamiński](https://twitter.com/Suiseki)
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,35 @@
 var current_word = document.querySelector('#chinese_char');
 var current_pron = document.querySelector('#pronunciation');
 var current_transl = document.querySelector('#translations');
+var char = document.querySelector('.char_btn');
+var draw_pron = document.querySelector('.pron_btn');
+var answer = document.querySelector('.answer_btn');
+var words_count = Object.keys(data.glossary.items).length;
+var current_id;
 
-console.log("test", data, current_transl);
+// console.log("test", words_count, current_transl);
 
+var draw_element = function () {
+	current_id = Math.floor(Math.random() * words_count);
+	console.log("current id: " + current_id);
+	current_transl.innerHTML = "…";
+	hide_answer(current_transl);
+	current_word.innerHTML = data.glossary.items[current_id].characters;
+	current_pron.innerHTML = data.glossary.items[current_id].pron;
+};
 
-current_transl.innerHTML = 'test';
-current_word.innerHTML = data.glossary.items[1].characters;
-current_pron.innerHTML = data.glossary.items[1].pron;
+var hide_answer = function (part) {
+	window.setTimeout(function(){
+		part.innerHTML = data.glossary.items[current_id].translation;
+	},6000);
+};
+
+var show_answer = function () {
+	current_transl.innerHTML = data.glossary.items[current_id].translation;
+
+}
+
+char.addEventListener('click', draw_element, false);
+draw_pron.addEventListener('click', draw_element, false);
+answer.addEventListener('click', show_answer, false);
 
