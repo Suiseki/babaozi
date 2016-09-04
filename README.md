3b33266ef1647f4d1636302d76941fd11f6abc56
Bartek <suiseki@gazeta.pl>
2016-09-04 09:28:48 +0200

    first btn, draw pronunciation, hide some elements before answer

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # BaBaozi - simple flashcards
-This is a simple app, based a flash version i wrote few year ago. All cards are stored in a text file. User clicks several buttons to draw a card and schow chinese characters, pronunciation or translation
+This is a simple app, based a flash version i wrote few year ago. All cards are stored in a text file. User clicks several buttons to draw a card and show chinese characters, pronunciation or translation
 
 ## Feature list
 - parse json character base
 - draw random chinese word
 - draw random pronunciation
 - draw random translation
+- show first, next and previous word in dictionary
 
 ### Credits
 * [Bartosz Kamiński](https://twitter.com/Suiseki)
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,46 +3,75 @@
 var current_word = document.querySelector('#chinese_char');
 var current_pron = document.querySelector('#pronunciation');
 var current_transl = document.querySelector('#translations');
+var current_itemid = document.querySelector('.word-item_id');
 var prev_btn = document.querySelector('.prev_btn');
+var first_btn = document.querySelector('.first_btn ');
 var next_btn = document.querySelector('.next_btn');
 var char = document.querySelector('.char_btn');
 var draw_pron = document.querySelector('.pron_btn');
 var answer = document.querySelector('.answer_btn');
 var words_count = Object.keys(data.glossary.items).length;
 var current_id =0;
+var later_answer;
+var data_pairs = {
+	"cword": [current_word, 'characters'],
+	"cpron": [current_pron, 'pron'],
+	"ctransl": [current_transl, 'translation']
+};
 
 // console.log("test", words_count, current_transl);
 
 var fill_output = function (fill_type) {
 
-
-	current_transl.innerHTML = '<hr class="progress">';
-	current_pron.innerHTML = '';
-	hide_answer(current_transl,current_pron);
-	current_word.innerHTML = data.glossary.items[current_id].characters;
+	//todo
+	if(fill_type === 'char'){
+		current_word.innerHTML = data.glossary.items[current_id].characters;
+		current_pron.innerHTML = '';
+		current_transl.innerHTML = '<hr class="progress">';
+		postpone_answer();
+		
+	} else if (fill_type === 0) {
+		console.log("pierwsze: " + current_id);
+		current_word.innerHTML = data.glossary.items[0].characters;
+		current_pron.innerHTML = data.glossary.items[current_id].pron;
+		current_transl.innerHTML = '<hr class="progress">';
+		postpone_answer();
+	} else {
+		current_word.innerHTML = '';
+		current_pron.innerHTML = data.glossary.items[current_id].pron;
+		current_transl.innerHTML = '<hr class="progress">';
+		postpone_answer();
+	}
+		current_itemid.innerHTML = '';
 
 }
 
-var draw_element = function () {
-	current_id = Math.floor(Math.random() * words_count);
+var draw_element = function (btn_hit) {
+
+	current_id = btn_hit !== 0 ? Math.floor(Math.random() * words_count) : 0;
 	console.log("current id: " + current_id);
-	fill_output();
+	fill_output(btn_hit);
 };
 
-var hide_answer = function (part, partb) {
-	window.setTimeout(function(){
-		part.innerHTML = data.glossary.items[current_id].translation;
-		partb.innerHTML = data.glossary.items[current_id].pron;
-	},7000);
-};
+var postpone_answer = function () {
+	later_answer = window.setTimeout(function(){
+		show_answer();
+	},6000);
+}
+
 
 var show_answer = function () {
+	current_word.innerHTML = data.glossary.items[current_id].characters;
 	current_pron.innerHTML = data.glossary.items[current_id].pron;
 	current_transl.innerHTML = data.glossary.items[current_id].translation;
+	current_itemid.innerHTML = data.glossary.items[current_id].item_id;
+	console.log("word: " + data.glossary.items[current_id].item_id, current_itemid);
+	clearTimeout(later_answer);
 
 }
 
 var make_step = function (step) {
+	//step +1 means forwards, -1 means backwards
 	current_id += (current_id < words_count) ? step : 0;
 	if (current_id<0){
 		current_id = words_count-1;
@@ -52,13 +81,14 @@ var make_step = function (step) {
 		}
 	}
 	console.log('next current_id: ',current_id);
-	fill_output();
+	fill_output('char');
 }
 
-fill_output();
-char.addEventListener('click', draw_element, false);
-draw_pron.addEventListener('click', draw_element, false);
+fill_output('char');
+char.addEventListener('click', draw_element.bind(null,'char'), false);
+draw_pron.addEventListener('click', draw_element.bind(null,'pron'), false);
 answer.addEventListener('click', show_answer, false);
-next_btn.addEventListener('click', make_step.bind(null,1), false);
 prev_btn.addEventListener('click', make_step.bind(null,-1), false);
+first_btn.addEventListener('click', draw_element.bind(null,0), false);
+next_btn.addEventListener('click', make_step.bind(null,1), false);
 
